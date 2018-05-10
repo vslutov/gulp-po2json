e0ea0279f00c1c3c57c8e5bde64e50abe2ac2137
vslutov <vslutov@yandex.ru>
2018-05-10 18:32:02 +0300

    v1.0.1

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const PluginError = require("plugin-error")
-const through = require("through2")
-const po2json = require("po2json")
-const path = require("path")
-const Vinyl = require("vinyl")
+const PluginError = require('plugin-error')
+const through = require('through2')
+const po2json = require('po2json')
+const Vinyl = require('vinyl')
 
-const pluginName = "gulp-po2json"
+const pluginName = 'gulp-po2json'
 
 module.exports = (config) => {
   config = Object.assign({
     stringify: true,
-    format: "jed1.x"
+    format: 'jed1.x'
   }, config)
 
   return through.obj(function (file, enc, cb) {
@@ -21,7 +20,7 @@ module.exports = (config) => {
     }
 
     if (file.isStream()) {
-      this.emit("error", new PluginError(pluginName, 'Streaming not supported'))
+      this.emit('error', new PluginError(pluginName, 'Streaming not supported'))
       return cb()
     }
 
@@ -29,19 +28,18 @@ module.exports = (config) => {
     file = new Vinyl({
       cwd: file.cwd,
       base: file.base,
-      path: file.path,
+      path: file.path
     })
-    file.extname = ".json"
+    file.extname = '.json'
 
     const domain = file.stem
     try {
-      const output_contents = po2json.parse(contents, Object.assign({}, config, {domain}), "utf8")
-      file.contents = Buffer.from(output_contents)
+      const outputContents = po2json.parse(contents, Object.assign({}, config, {domain}), 'utf8')
+      file.contents = Buffer.from(outputContents)
     } catch (err) {
-      this.emit('error', new PluginError(pluginName, err));
-      return cb();
+      this.emit('error', new PluginError(pluginName, err))
+      return cb()
     }
-
 
     this.push(file)
     cb()
--- test/main.js
+++ test/main.js
@@ -1,59 +1,59 @@
-'use strict';
+'use strict'
 
-const po2json = require('po2json');
-const gulpPo2json = require('../');
-const Vinyl = require('vinyl');
-const expect = require('chai').expect;
-const fs = require('fs');
-const path = require('path');
+const po2json = require('po2json')
+const gulpPo2json = require('../')
+const Vinyl = require('vinyl')
+const expect = require('chai').expect
+const fs = require('fs')
+const path = require('path')
 
 const createFile = function (contents) {
-  const base = path.normalize(__dirname + '/../fixtures');
+  const base = path.normalize(path.resolve(__dirname, '../fixtures'))
   return new Vinyl({
     path: path.join(base, 'es.po'),
     base: base,
     cwd: __dirname,
     contents: contents
-  });
-};
+  })
+}
 
 describe('gulp-po2json', function () {
   it('should match po2json output', function (done) {
-    po2json.parseFile(__dirname + '/fixtures/es.po', {stringify: true}, function (err, data) {
+    po2json.parseFile(path.join(__dirname, 'fixtures/es.po'), {stringify: true}, function (err, data) {
       if (err) {
-        done(err);
-        return;
+        done(err)
+        return
       }
 
-      fs.readFile(__dirname + '/fixtures/es.po', function (err, esPo) {
+      fs.readFile(path.join(__dirname, 'fixtures/es.po'), function (err, esPo) {
         if (err) {
-          done(err);
-          return;
+          done(err)
+          return
         }
         gulpPo2json()
           .on('error', done)
           .on('data', function (file) {
-            expect(file.isNull()).to.be.false;
-            expect(file.contents.toString()).to.equal(data);
+            expect(file.isNull()).to.be.false
+            expect(file.contents.toString()).to.equal(data)
 
-            done();
+            done()
           })
-          .write(createFile(esPo));
-      });
-    });
-  });
+          .write(createFile(esPo))
+      })
+    })
+  })
 
   it('should handle po2json errors', function (done) {
     gulpPo2json()
       .on('error', function (err) {
-        expect(err).to.be.ok;
-        expect(err).to.be.an.instanceOf(Error);
-        expect(err.plugin).to.equal('gulp-po2json');
-        done();
+        expect(err).to.be.ok
+        expect(err).to.be.an.instanceOf(Error)
+        expect(err.plugin).to.equal('gulp-po2json')
+        done()
       })
       .write(new Vinyl({
         path: '/tmp/foo.po',
         contents: Buffer.from('msgid')
-      }));
-  });
-});
+      }))
+  })
+})
